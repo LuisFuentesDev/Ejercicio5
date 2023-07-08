816e31b97b96159f812479fbd9239fe2f8c7366a
LuisFuentesDev <lsfuentes2@gmail.com>
2023-07-07 20:19:20 -0400

    Falta todo

---
+++ app/src/main/java/com/example/ejercicio5/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.ejercicio5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ejercicio5.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    public ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
